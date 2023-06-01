beb1be0a8cfe093df427542c403f8c88a1b4065f
Samuel <jsscript001@gmail.com>
2023-06-01 14:52:22 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function toggleMode() { 
+    const html = document.documentElement
+    html.classList.toggle("light")
+    // if(html.classList.contains('light')) {
+    // html.classList.remove('light')
+    // } else {
+    // html.classList.add("light")
+    // }
+
+    //como mudar a Imagem. abaixo
+
+    // //pegar a tag img 
+    // const img = document.querySelector("#profile img")
+    // // substituir a imagem
+    // if(html.classList.contains("light")) {
+    //     img.setAttribute("src", "./assets/Perfil quadrada.webp")
+    // } else {
+    //     img.setAttribute("src", "./assets/Perfil redonda.png")
+    // }
+    
+
+}   
